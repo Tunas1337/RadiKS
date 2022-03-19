eabe5619176d0e001c566d570fefc7d19192426b
Andrej Antunovikj <tunas@cryptolab.net>
2022-03-19 16:14:11 -0400

    Changed display_light to be more verbose

--- ion/src/device/flasher/display_light.cpp
+++ ion/src/device/flasher/display_light.cpp
@@ -1,17 +1,52 @@
 #include <ion.h>
 #include <kandinsky.h>
+#include "image.h"
 
 namespace Flasher {
-namespace Display {
+    namespace Display {
 
-void init() {
-  KDRect screen = KDRect(0,0,Ion::Display::Width,Ion::Display::Height);
-  Ion::Display::pushRectUniform(KDRect(0,0,Ion::Display::Width,Ion::Display::Height), KDColor::RGB24(0x5e81ac));
-  KDContext * ctx = KDIonContext::sharedContext();
-  ctx->setOrigin(KDPointZero);
-  ctx->setClippingRect(screen);
-  ctx->drawString("RECOVERY MODE", KDPoint(10, 10), KDFont::LargeFont, KDColorWhite, KDColor::RGB24(0x5e81ac));
-}
+        constexpr static int sNumberOfMessages = 3;
 
-}
+        constexpr static const char * sMessages[sNumberOfMessages] = {
+            "Jao! Numworks is dead!",
+            "Please plug in your NumWorks",
+            "and flash the latest firmware.",
+        };
+
+        void init() {
+            KDRect screen = KDRect(0,0,Ion::Display::Width,Ion::Display::Height);
+            Ion::Display::pushRectUniform(screen, KDColor::RGB24(0x2B2B2B));
+            KDContext * ctx = KDIonContext::sharedContext();
+            ctx->setOrigin(KDPointZero);
+            ctx->setClippingRect(screen);
+            KDCoordinate margin = 30;
+            KDCoordinate currentHeight = margin;
+
+            /* Title */
+            const char * title = sMessages[0];
+            KDSize titleSize = KDFont::LargeFont->stringSize(title);
+            ctx->drawString(title, KDPoint((Ion::Display::Width-titleSize.width())/2, currentHeight),
+                            KDFont::LargeFont, KDColorWhite, KDColor::RGB24(0x2B2B2B));
+            currentHeight = (uint16_t)((Ion::Display::Height*2)/3);
+ 
+            for (int i = 0; i < IMAGE_WIDTH; ++i) {
+                for (int j = 0; j < IMAGE_HEIGHT; ++j) {
+                    ctx->setPixel(KDPoint(i+(uint16_t)((Ion::Display::Width-IMAGE_WIDTH)/2),
+                                          j+(titleSize.height()+margin+15)),
+                                  KDColor::RGB16(image[i+(j*IMAGE_WIDTH)]));
+                }
+            }
+
+            /* Messages */
+            const char * message;
+            for (int i = 1; i < sNumberOfMessages; ++i) {
+                message = sMessages[i];
+                KDSize messageSize = KDFont::SmallFont->stringSize(message);
+                ctx->drawString(message, KDPoint((Ion::Display::Width-messageSize.width())/2, currentHeight),
+                                KDFont::SmallFont, KDColorWhite, KDColor::RGB24(0x2B2B2B));
+                currentHeight += messageSize.height();
+            }
+        }
+
+    }
 }
